f0ed2c485fc2b92f7c4afdfd2902013ceb438879
rngesus_exe <behzadshaheryar@gmail.com>
2022-01-21 05:05:03 +0500

    Created Signup/Login Screen

--- app/src/main/java/com/rngesus/mywallet/MainActivity.java
+++ app/src/main/java/com/rngesus/mywallet/MainActivity.java
@@ -1,14 +1,113 @@
 package com.rngesus.mywallet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber, mCode;
+    private Button mSend;
+
+    private String mVerificationId;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mCode = findViewById(R.id.code);
+
+        mSend = findViewById(R.id.send);
+
+        mSend.setOnClickListener(view -> {
+            if(!mPhoneNumber.getText().toString().isEmpty()) {
+                if (mVerificationId != null) {
+                    VerifyPhoneNumberWithCode();
+                }
+                startPhoneNumberVerification();
+            }
+            else{
+                Toast.makeText(this, "Enter a Phone Number", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                SignInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                DisplayVerificationError();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(verificationId, forceResendingToken);
+
+                mVerificationId = verificationId;
+                mSend.setText("Verify Code");
+            }
+        };
+
+    }
+
+    private void DisplayVerificationError() {
+        Toast.makeText(this, "Incorrect Verification Code", Toast.LENGTH_SHORT).show();
+    }
+
+    private void VerifyPhoneNumberWithCode(){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId,mCode.getText().toString());
+    }
+
+    private void SignInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this,
+                task -> {
+                    if(task.isSuccessful()){
+                        userIsLoggedIn();
+                    }
+                });
+    }
+
+    private void userIsLoggedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            startActivity(new Intent(getApplicationContext(),MainPageActivity.class));
+            finish();
+        }
+    }
+
+    private void startPhoneNumberVerification() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                mCallbacks
+        );
     }
 }
