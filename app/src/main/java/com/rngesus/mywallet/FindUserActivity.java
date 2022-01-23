45a943894a0ffee1f165e38de5f63285cd39b9d8
rngesus_exe <behzadshaheryar@gmail.com>
2022-01-24 02:33:18 +0500

    Now only Contacts which installed app will show up in "FindUserActivity"

--- app/src/main/java/com/rngesus/mywallet/FindUserActivity.java
+++ app/src/main/java/com/rngesus/mywallet/FindUserActivity.java
@@ -1,5 +1,6 @@
 package com.rngesus.mywallet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,7 +9,15 @@ import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.telephony.TelephonyManager;
 import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -18,29 +27,86 @@ public class FindUserActivity extends AppCompatActivity {
     private RecyclerView.Adapter mUserListAdapter;
     private RecyclerView.LayoutManager mUserListLayoutManager;
 
-    ArrayList<UserObject> userList;
+    ArrayList<UserObject> userList, contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
 
-        userList = new ArrayList<>();
+        contactList= new ArrayList<>();
+        userList= new ArrayList<>();
 
         initializeRecyclerView();
+        getContactList();
     }
 
     @SuppressLint("NotifyDataSetChanged")
     private void getContactList(){
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null,null);
+        String ISOPrefix = getCountryISO();
+        @SuppressLint("Recycle") Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null,null);
         while(phones.moveToNext()){
             @SuppressLint("Range") String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             @SuppressLint("Range") String phone = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
+            phone = phone.replace(" ", "");
+            phone = phone.replace("-", "");
+            phone = phone.replace("(", "");
+            phone = phone.replace(")", "");
+
+            if(!String.valueOf(phone.charAt(0)).equals("+")){
+                phone = ISOPrefix + phone;
+            }
+
             UserObject mContact = new UserObject(name,phone);
-            userList.add(mContact);
-            mUserListAdapter.notifyDataSetChanged();
+            contactList.add(mContact);
+            getUserDetails(mContact);
         }
+    }
+
+    private void getUserDetails(UserObject mContact) {
+        DatabaseReference mUserDB = FirebaseDatabase.getInstance().getReference().child("user");
+        Query query = mUserDB.orderByChild("phone").equalTo(mContact.getPhone());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String phone = "",
+                            name = "";
+                    for(DataSnapshot childSnapShot : snapshot.getChildren()){
+                        if(childSnapShot.child("phone").getValue() != null){
+                            phone = childSnapShot.child("phone").getValue().toString();
+                        }
+                        if(childSnapShot.child("name").getValue() != null){
+                            name = childSnapShot.child("name").getValue().toString();
+                        }
+
+                        UserObject mUser = new UserObject(name, phone);
+                        userList.add(mUser);
+                        mUserListAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private String getCountryISO(){
+        String iso = null;
+
+        TelephonyManager telephonyManager = (TelephonyManager) getApplicationContext().getSystemService(getApplicationContext().TELEPHONY_SERVICE);
+        if(telephonyManager.getNetworkCountryIso() != null){
+            if(telephonyManager.getNetworkCountryIso().toString().equals("")){
+                iso = telephonyManager.getNetworkCountryIso().toString();
+            }
+        }
+
+        return CountryToPhonePrefix.getPhone(iso);
     }
 
     @SuppressLint("WrongConstant")
--- app/src/main/java/com/rngesus/mywallet/MainPageActivity.java
+++ app/src/main/java/com/rngesus/mywallet/MainPageActivity.java
@@ -6,7 +6,6 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,7 +22,7 @@ public class MainPageActivity extends AppCompatActivity {
 
         mLogout.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
             finish();
