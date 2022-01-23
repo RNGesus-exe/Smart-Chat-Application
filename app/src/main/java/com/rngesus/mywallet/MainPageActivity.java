193141e5e40930e2e68aec3c0667a69fdff1daac
rngesus_exe <behzadshaheryar@gmail.com>
2022-01-23 19:01:55 +0500

    Added "Contacts Activity"

--- app/src/main/java/com/rngesus/mywallet/MainPageActivity.java
+++ app/src/main/java/com/rngesus/mywallet/MainPageActivity.java
@@ -2,7 +2,9 @@ package com.rngesus.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,8 @@ public class MainPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
 
         Button mLogout = findViewById(R.id.logout);
+        Button mFindUser = findViewById(R.id.findUser);
+
         mLogout.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
@@ -25,5 +29,14 @@ public class MainPageActivity extends AppCompatActivity {
             finish();
         });
 
+        mFindUser.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(),FindUserActivity.class)));
+
+        getPermissions();
+    }
+
+    private void getPermissions() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS},1);
+        }
     }
 }
---
+++ app/src/main/java/com/rngesus/mywallet/UserObject.java
@@ -0,0 +1,19 @@
+package com.rngesus.mywallet;
+
+public class UserObject {
+    private String name;
+    private String phone;
+
+    public UserObject(String name,String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
